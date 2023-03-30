f17ffb8f3975c27a426e4d481b3f6c0f2707556b
PC677 <abdujafar.rashidov.2006@gmail.com>
2023-03-30 11:57:46 +0500

    Fragment Switcher

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container, HomeFragment())
+            .commit()
+
+        binding.homeFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, HomeFragment())
+                .commit()
+        }
+
+        binding.profileFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, ProfileFragment())
+                .commit()
+        }
+
+
+
+    }
+}
