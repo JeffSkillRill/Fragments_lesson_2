f716510465669779db926d116ad0bdd17db52a5c
PC677 <abdujafar.rashidov.2006@gmail.com>
2023-03-30 13:07:04 +0500

    Fragment Bundle

--- app/src/main/java/com/example/myapplication/CabinetFragment.kt
+++ app/src/main/java/com/example/myapplication/CabinetFragment.kt
@@ -5,20 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.myapplication.databinding.FragmentCabinetBinding
+import com.example.myapplication.databinding.FragmentProfileBinding
+
+// TODO: Rename parameter arguments, choose names that match
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
 
 class CabinetFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
-
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cabinet, container, false)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
     }
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = FragmentCabinetBinding.inflate(inflater, container, false)
 
+        val names = binding.name.text
+        val password = binding.password.text
+        val t = parentFragmentManager.beginTransaction()
+
+        binding.enter.setOnClickListener{
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.profile_main, MyOrderFragment.newInstance(names.toString(), password.toString()))
+                .commit()
+        }
+
+        return binding.root
+    }
+
+    companion object {
+
+        @JvmStatic fun newInstance(param1: String, param2: String) =
+                CabinetFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
+                }
+    }
 }
--- app/src/main/java/com/example/myapplication/MyOrderFragment.kt
+++ app/src/main/java/com/example/myapplication/MyOrderFragment.kt
@@ -5,17 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.myapplication.databinding.FragmentMyOrderBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MyOrderFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class MyOrderFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -33,19 +29,16 @@ class MyOrderFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_order, container, false)
+        val binding = FragmentMyOrderBinding.inflate(inflater, container, false)
+        val order = MyOrderFragment()
+        val name = param1
+        val password = param2
+        binding.user.text = name+" "+password
+        return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyOrderFragment.
-         */
+
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
